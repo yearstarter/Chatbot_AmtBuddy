e1a3785dabd604518a64571a1fc201af5987daca
yearstarter <135331245+yearstarter@users.noreply.github.com>
2023-10-19 19:46:55 +0200

    Update AmtBuddy.py

--- AmtBuddy.py
+++ AmtBuddy.py
@@ -32,8 +32,6 @@ from PyPDF2 import PdfReader
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
-from langchain.chains.question_answering import load_qa_chain
-from langchain.callbacks import get_openai_callback
 from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
 
 from apikeys import apikey_OpenAI
